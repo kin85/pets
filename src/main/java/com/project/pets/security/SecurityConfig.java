08f9a75bedefb0dd4b3550cacca4ad9300f8dd85
Joaquin Fernandez <joaquin@MacBook-Air-de-Sofia.local>
2026-04-06 12:18:46 +0200

    add url for cors

--- src/main/java/com/project/pets/security/SecurityConfig.java
+++ src/main/java/com/project/pets/security/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://pets-black-ten.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
